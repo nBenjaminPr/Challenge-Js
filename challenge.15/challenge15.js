9b3c3b0359c2def6c33f47252e0039b6bfacf028
Nicolás Benjamín Pereira <112033389+nBenjaminPr@users.noreply.github.com>
2022-09-19 20:57:20 -0300

    Add files via upload

---
+++ challenge.15/challenge15.js
@@ -0,0 +1,13 @@
+function lanzamiento1(){
+    var dado1 = document.getElementById("dado1");
+    var d1 = Math.floor(Math.random() * 6) + 1 ;
+    dado1.innerHTML = d1;
+    
+}
+
+function lanzamiento2(){
+    var dado2 = document.getElementById("dado2");
+    var d2 = Math.floor(Math.random() * 6) + 1;
+    dado2.innerHTML = d2;
+    ;
+}
