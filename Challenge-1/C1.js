d544ca34eb3f530a75b8faff76202a0aaf6dca2e
Nicolas <nicobenpe@gmail.com>
2023-03-29 20:37:03 -0300

    Practicando JavaScript en la consola y en el documento

---
+++ Challenge-1/C1.js
@@ -0,0 +1,49 @@
+document.write("hola mundo")
+
+"/br"
+
+// Inicio 2
+
+let a, b;
+a=10; b=5;
+resultado = a + b;
+
+console.log(`La suma de a+b es ${resultado}`)
+
+
+// Inicio 3
+
+let num1=0;
+let num2=0;
+
+num1 = num1+1;
+num2 = num2+5;
+
+console.log(`El primer numero ahora es ${num1} `); 
+console.log(`El segundo numero ahora es ${num2}`);
+
+// Inicio 4
+
+const name1 = "hola";
+let name2 = " mundo!";
+
+document.write ("</br>")
+document.write ("</br>")
+document.write("Constante 1 contine " + name1);
+document.write ("</br>")
+document.write ("</br>")
+document.write("Variable 2 contine " + name2)
+
+//Inicio 5
+
+let c, d ;
+c = 2; d = 8;
+resultado2 = c * d
+
+document.write ("</br>")
+document.write ("</br>")
+document.write (`Variable c contiene ${c} <br> `)
+document.write (`Variable d contiene ${d} <br> `)
+document.write (`El producto de c por d es ${resultado2}`)
+
+
