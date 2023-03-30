b8ba62816e93571fb8eb7d3b3624701f6d01d485
Nicolas <nicobenpe@gmail.com>
2023-03-30 19:44:00 -0300

    Practicando la logica de JS

--- Challenge-1/C1.js
+++ Challenge-1/C1.js
@@ -46,4 +46,39 @@ document.write (`Variable c contiene ${c} <br> `)
 document.write (`Variable d contiene ${d} <br> `)
 document.write (`El producto de c por d es ${resultado2}`)
 
+//inicio 6
 
+let dato, resultado3;
+
+// dato = window.prompt ("introduce tu nombre", "0");
+resultado3 = `hola, como estas ${dato}` ;
+document.write ("</br")
+console.log(resultado3);
+
+
+//inicio 7
+
+let dato2, num3;
+
+// dato2 = window.prompt("Introduce numero ?", "0");
+num3 = parseInt(dato2) //Transforma el texto introducido en un valor numerico
+
+num3 = num3 *2;
+
+console.log(`El doble es ${num3} `);
+
+
+//inicio 8
+let dato6,dato7, num4,num5;
+
+// dato6 = window.prompt ("Introduce primer numero ?", "0")
+// num4 = parseInt(dato6);
+// dato7 = window.prompt ("iNTRODUCE EL SEGUNDO NUMERO ?" , "0");
+// num5 = parseInt(dato7);
+
+let resultado4 = num4 + num5
+
+console.log(`La suma es ${resultado4}`);
+
+
+//Inicio 9
