5ebea4480f10a24fc92eb4e18809e86795acb9b4
Nicolas <nicobenpe@gmail.com>
2023-04-02 20:20:58 -0300

    Practicando condicionales con if and else

--- Challenge-2/condicionales.js
+++ Challenge-2/condicionales.js
@@ -64,16 +64,52 @@
 
 //Condicionales 4
 
-let suerte = (Math.random() *6);
-let dado = parseInt(suerte) + 1;
+// let suerte = (Math.random() * 6);
+// let dado = parseInt(suerte) + 1;
 
-let obj1= document.getElementById("salida")
+// let obj1= document.getElementById("salida")
 
-if (dado > 3){
-    obj1.innerHTML= (`Has ganado ${dado}`)
-}{
-    obj1.innerHTML= (`segui participando ${dado}`)
-}
+// if (dado > 3){
+//     obj1.innerHTML= (`Has ganado ${dado}`)
+// }{
+//     obj1.innerHTML= (`segui participando ${dado}`)
+// }
+
+
+//Condicionales 5
+
+// let marca, modelo, descuento = 0
+
+// alert ("Eliga su auto Ford fiesta o focus")
+
+// marca = prompt("MARCA?")
+// modelo= prompt("modelo?")
+
+// if ((marca == "ford") && (modelo="fiesta")){
+//     descuento = 5;
+// }
+
+// if ((marca == "ford") && (modelo = "focus")){
+//     descuento = 10
+// }
+
+// document.write(`<h3> Su descuento es ${descuento} </h3>`)
+
+//Condicionales 5
+
+// let alumno, edad, acceso
+
+// alumno = prompt (" Es de FP?")
+// edad = prompt ("Cuanto años tenes?")
+
+// if ((alumno == "si") && (edad > 18)){
+//         acceso = "si tiene acceso"
+//     }else if ((alumno == "si") && (edad < 18)) {
+//         acceso = "no tiene acceso"
+//     }
 
 
 
+// document.write(`<h3> Usted ${acceso} </h3>`)
+
+//Condicionales 6
