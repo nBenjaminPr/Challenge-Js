7dec1e989bfc8fc68af929ce718c38555f242c1d
Nicolas <nicobenpe@gmail.com>
2023-04-10 21:18:22 -0300

    Añadiendo funciones de calculo

--- Challenge-4/Forms.js
+++ Challenge-4/Forms.js
@@ -14,3 +14,11 @@ function descuento() {
 
     document.writeln(`<h3> El 10% de ${descuento_result} es ${resutlado} </h3>`)
 }
+
+
+// Form 3
+
+function calcular (){
+    // eval: una expresión de JavaScript que toma el valor introducido por el usuario en un formulario HTML y lo evalúa como una expresión JavaScript.
+    document.datos.respuesta.value = eval(document.datos.entrada2.value)
+}
