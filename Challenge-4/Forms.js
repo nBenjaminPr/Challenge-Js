982f72f25fab9228a3a0b59e69ac92079882d684
Nicolas <nicobenpe@gmail.com>
2023-04-10 21:10:15 -0300

    Boton que da el 10% de su valor

--- Challenge-4/Forms.js
+++ Challenge-4/Forms.js
@@ -1,4 +1,16 @@
+// Fomrs 1
+
+
 function saludo() {
     const nom_usuario = document.fdatos.entrada.value
     document.writeln(`<h3> Hola, Buen dia ${nom_usuario} </h3>`)
+}
+
+// Fomrs 2
+
+function descuento() {
+    const descuento_result = document.fnum.entradas.value
+    let resutlado = descuento_result *0.1
+
+    document.writeln(`<h3> El 10% de ${descuento_result} es ${resutlado} </h3>`)
 }
