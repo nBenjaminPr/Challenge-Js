999b0b87ba8e6ac048c94dd2eb365dff42e5cf1b
Nicolas <nicobenpe@gmail.com>
2023-04-16 19:43:41 -0300

    Trabajando con 2 variables

--- Challenge-5/Bucles.js
+++ Challenge-5/Bucles.js
@@ -10,3 +10,19 @@ for (i=0; i<10; i++)
 document.getElementById("central1").innerHTML= cadena
 
 // Blucles 2
+
+let x,j;
+let z = "<table>"
+
+for (x =1; x <= 5 ;x++) {
+    z = z + "<tr>";
+    for (j = 1; j <= x; j++)
+    z = z + `<td> El numero es ${x} </td>`;
+
+    z = z + "<tr>"
+}
+
+z = z + "</table>"
+document.getElementById("central2").innerHTML = z
+
+// Blucles 3
