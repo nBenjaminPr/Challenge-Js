4940b8cc8f69670a1424ee2284a377e9fa97887a
Nicolas <nicobenpe@gmail.com>
2023-04-16 19:31:47 -0300

    Empenzando con bucles

---
+++ Challenge-5/Bucles.js
@@ -0,0 +1,12 @@
+// Blucles 1
+
+let cadena = "", i;
+
+for (i=0; i<10; i++)
+{
+    cadena = cadena + `<h3> El numero es ${i} </h3>`
+}
+
+document.getElementById("central1").innerHTML= cadena
+
+// Blucles 2
