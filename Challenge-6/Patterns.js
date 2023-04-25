2d9a5f160489dfecf21f08395d06b4ed87ef0d10
Nicolas <nicobenpe@gmail.com>
2023-04-25 20:17:10 -0300

    practicando constructor

--- Challenge-6/Patterns.js
+++ Challenge-6/Patterns.js
@@ -21,3 +21,49 @@ function programa(){
     document.write (`<td> El nombre es ${obj.verNombre()} </td>`)
     document.write (`<td> La edad es ${obj.varEdad()} </td>`);
 }
+
+
+
+function ficha2() {
+
+    var alumno = {
+    nombre: "Caterina",
+    edad: 18
+    };
+
+    this.verNombre = function ()
+    {
+    return alumno.nombre;
+    }
+
+    this.verEdad = function ()
+    {
+    var resultado = alumno.edad;
+    return resultado;
+    }
+}
+function programas() {
+    var obj = new ficha2();
+
+    document.writeln("El nombre es "+ obj.verNombre() + "<br>");
+    document.writeln("La edad es "+ obj.verEdad() + "<br>");
+} 
+
+
+
+//Patterns 2
+
+class animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    decirNombre(){
+        console.log("Soy " + this.name);
+    }
+}
+
+var obj1 = new animal("leon")
+obj1.decirNombre();
+var obj2 = new animal("toro")
+obj2.decirNombre();
